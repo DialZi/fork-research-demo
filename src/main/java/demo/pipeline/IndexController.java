e921bdfb5db4e800649bf38b65d0d1c63506373d
Dominik Meierhofer <dummy8006@outlook.com>
2021-01-17 22:23:17 +0100

    add database

--- src/main/java/demo/pipeline/IndexController.java
+++ src/main/java/demo/pipeline/IndexController.java
@@ -1,5 +1,11 @@
 package demo.pipeline;
 
+import java.sql.Statement;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.ResultSet;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,8 +14,57 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping(value = { "/index", "/" })
 public class IndexController {
 
+    // JDBC driver name and database URL
+    static final String JDBC_DRIVER = "org.h2.Driver";
+    static final String DB_URL = "jdbc:h2:mem:testdb";
+
+    // Database credentials
+    static final String USER = "sa";
+    static final String PASS = "password";
+
     @GetMapping
     public String index() {
+
+        Connection conn = null;
+        Statement stmt = null;
+        try {
+            // STEP 1: Register JDBC driver
+            Class.forName(JDBC_DRIVER);
+
+            // STEP 2: Open a connection
+            System.out.println("Connecting to database...");
+            conn = DriverManager.getConnection(DB_URL, USER, PASS);
+
+            // STEP 3: Execute a query
+            stmt = conn.createStatement();
+            String sql = "SELECT * FROM billionaires";
+            ResultSet rs = stmt.executeQuery(sql);
+
+            // STEP 4: Clean-up environment
+            stmt.close();
+            conn.close();
+        } catch (SQLException se) {
+            // Handle errors for JDBC
+            se.printStackTrace();
+        } catch (Exception e) {
+            // Handle errors for Class.forName
+            e.printStackTrace();
+        } finally {
+            // finally block used to close resources
+            try {
+                if (stmt != null)
+                    stmt.close();
+            } catch (SQLException se2) {
+            } // nothing we can do
+            try {
+                if (conn != null)
+                    conn.close();
+            } catch (SQLException se) {
+                se.printStackTrace();
+            } // end finally try
+        } // end try
+        System.out.println("Goodbye!");
+
         return "index";
     }
 
